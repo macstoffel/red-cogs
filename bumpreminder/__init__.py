e4f5c5cf003486f1a1ea9bfcb6b42018f625ec15
macstoffel <macstoffel@icloud.com>
2025-10-30 12:34:44 +0100

    aa

--- bumpreminder/__init__.py
+++ bumpreminder/__init__.py
@@ -1,4 +1,7 @@
-from .bumpreminder import BumpReminder
+# Re-export the cog setup so Red can await package.setup(bot)
 
-def setup(bot):
-    bot.add_cog(BumpReminder(bot))
+from .bumpreminder import BumpReminder  # import the cog class
+
+async def setup(bot):
+    """Add the BumpReminder cog to the bot."""
+    await bot.add_cog(BumpReminder(bot))
